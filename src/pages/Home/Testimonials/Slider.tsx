9cb163e89a553d08a48950ca87ad1966f2dd2c0f
Ekaterina <pandafomka@gmail.com>
2025-09-14 19:19:34 +0300

    feat: testimonials section

--- src/pages/Home/Testimonials/Slider.tsx
+++ src/pages/Home/Testimonials/Slider.tsx
@@ -3,11 +3,36 @@ import avatar from "./avatar.png";
 import "swiper/swiper-bundle.css";
 import styles from "./Testimonials.module.scss";
 import { Quote } from "@/shared/images/icons/Quote";
+import { Autoplay, Pagination } from "swiper/modules";
 
 export const Slider = () => {
   return (
     <div className={styles.slider}>
-      <Swiper spaceBetween={30} slidesPerView={2.3}>
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        pagination={{
+          bulletClass: styles.bullet,
+          bulletActiveClass: styles.bullet_active,
+        }}
+        spaceBetween={30}
+        slidesPerView={2.3}
+        autoplay={{ delay: 3000 }}
+      >
+        <SwiperSlide>
+          <SliderItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SliderItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SliderItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SliderItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SliderItem />
+        </SwiperSlide>
         <SwiperSlide>
           <SliderItem />
         </SwiperSlide>
@@ -26,11 +51,16 @@ const SliderItem = () => {
   return (
     <article className={styles.item}>
       <div className={styles.header}>
-        <img src={avatar} alt="avatar"></img>
-        <div className={styles.name}></div>
+        <div className={styles.user}>
+          <img src={avatar} alt="avatar" className={styles.avatar}></img>
+          <div className={styles.info}>
+            <div className={styles.name}>John Doe</div>
+            <div className={styles.location}>USA, America</div>
+          </div>
+        </div>
         <Quote />
       </div>
-      <p>
+      <p className={styles.text}>
         “ Lorem Ipsum has been the industry's standard dummy text ever since the
         1500s, when an unknown printer took a galley of type and scrambled it to
         make a type specimen book. ”
